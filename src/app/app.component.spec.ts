23f788ae3a8fb6e668f3dde9c0358deab8500f09
henriquewallace <henrique.wallace@hotmail.com>
2022-05-18 15:16:43 -0300

    Refactor all css style using bootstrap

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'course-angular-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('course-angular-project');
+    expect(app).toEqual('course-angular-project');
   });
 
   it('should render title', () => {
