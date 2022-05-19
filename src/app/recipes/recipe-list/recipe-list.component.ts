a112aebffbbc585c44a8d7f0aff7a374ee1b5efe
henriquewallace <henrique.wallace@hotmail.com>
2022-05-18 21:32:59 -0300

    Fill the recipe list with some test rescipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ import { Recipe } from "../recipe.model";
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is simply a test', 'https://pixabay.com/images/id-4813261/')
+    new Recipe('A test recipe', 'This is simply a test', 'https://st2.depositphotos.com/48197816/46110/i/1600/depositphotos_461109044-stock-photo-heat-skillet-parallel-little-oil.jpg'),
+    new Recipe('Test 2', 'This a test2', 'https://st4.depositphotos.com/24147632/38096/i/1600/depositphotos_380961580-stock-photo-fresh-homemade-chocolate-chip-cookies.jpg')
   ];
 
   constructor() { }
